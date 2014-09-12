5d2ad16f5d33505aa7f83b6a6cf52dfceeaeff76
Margaret Feltz <margaret.feltz@tufts.edu>
2014-09-12 17:05:06 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,137 @@
+$(document).ready(function(){
+
+//Hover over eboard people
+$(".headshots").on("mouseenter", function(){
+	$(".default").hide();
+}).on("mouseleave", function() {
+	$(".default").show();
+});
+//MARGARET
+$(".margaret").on("mouseenter", function(){
+	$(".margaret_info").show();
+}).on("mouseleave", function() {
+	$(".margaret_info").hide();
+});
+//BILL
+$(".bill").on("mouseenter", function(){
+	$(".bill_info").show();
+}).on("mouseleave", function() {
+	$(".bill_info").hide();
+});
+//AMANDA
+$(".amanda").on("mouseenter", function(){
+	$(".amanda_info").show();
+}).on("mouseleave", function() {
+	$(".amanda_info").hide();
+});
+//EVELYN
+$(".evelyn").on("mouseenter", function(){
+	$(".evelyn_info").show();
+}).on("mouseleave", function() {
+	$(".evelyn_info").hide();
+});
+//RUOXI
+$(".ruoxi").on("mouseenter", function(){
+	$(".ruoxi_info").show();
+}).on("mouseleave", function() {
+	$(".ruoxi_info").hide();
+});
+//SARA
+$(".sara").on("mouseenter", function(){
+	$(".sara_info").show();
+}).on("mouseleave", function() {
+	$(".sara_info").hide();
+});
+//THOMAS
+$(".thomas").on("mouseenter", function(){
+	$(".thomas_info").show();
+}).on("mouseleave", function() {
+	$(".thomas_info").hide();
+});
+//SIYI
+$(".siyi").on("mouseenter", function(){
+	$(".siyi_info").show();
+}).on("mouseleave", function() {
+	$(".siyi_info").hide();
+});
+//AUDREY
+$(".audrey").on("mouseenter", function(){
+	$(".audrey_info").show();
+}).on("mouseleave", function() {
+	$(".audrey_info").hide();
+});
+//LYNN
+$(".lynn").on("mouseenter", function(){
+	$(".lynn_info").show();
+}).on("mouseleave", function() {
+	$(".lynn_info").hide();
+});
+//BRENDA
+$(".brenda").on("mouseenter", function(){
+	$(".brenda_info").show();
+}).on("mouseleave", function() {
+	$(".brenda_info").hide();
+});
+//VANESSA
+$(".vanessa").on("mouseenter", function(){
+	$(".vanessa_info").show();
+}).on("mouseleave", function() {
+	$(".vanessa_info").hide();
+});
+//AMELIA
+$(".amelia").on("mouseenter", function(){
+	$(".amelia_info").show();
+}).on("mouseleave", function() {
+	$(".amelia_info").hide();
+});
+//ALI
+$(".ali").on("mouseenter", function(){
+	$(".ali_info").show();
+}).on("mouseleave", function() {
+	$(".ali_info").hide();
+});
+//MARK
+$(".mark").on("mouseenter", function(){
+	$(".mark_info").show();
+}).on("mouseleave", function() {
+	$(".mark_info").hide();
+});
+//CATIE
+$(".catie").on("mouseenter", function(){
+	$(".catie_info").show();
+}).on("mouseleave", function() {
+	$(".catie_info").hide();
+});
+//ADIEL
+$(".adiel").on("mouseenter", function(){
+	$(".adiel_info").show();
+}).on("mouseleave", function() {
+	$(".adiel_info").hide();
+});
+//DEREK
+$(".derek").on("mouseenter", function(){
+	$(".derek_info").show();
+}).on("mouseleave", function() {
+	$(".derek_info").hide();
+});
+//NATASHA
+$(".natasha").on("mouseenter", function(){
+	$(".natasha_info").show();
+}).on("mouseleave", function() {
+	$(".natasha_info").hide();
+});
+//YIJA
+$(".yija").on("mouseenter", function(){
+	$(".yija_info").show();
+}).on("mouseleave", function() {
+	$(".yija_info").hide();
+});
+//WILL
+$(".will").on("mouseenter", function(){
+	$(".will_info").show();
+}).on("mouseleave", function() {
+	$(".will_info").hide();
+});
+
+});
+
